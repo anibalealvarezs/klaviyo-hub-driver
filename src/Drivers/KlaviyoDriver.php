d43fd7bf811d8dbf986f13bedb9c8e4c13d8b1e0
Anibal Alvarez <aalvarez@chmw.co>
2026-04-13 17:37:31 -0400

    feat: enforce disabled-by-default and implement core hydration

--- src/Drivers/KlaviyoDriver.php
+++ src/Drivers/KlaviyoDriver.php
@@ -356,7 +356,7 @@ class KlaviyoDriver implements SyncDriverInterface
     {
         return [
             'global' => [
-                'enabled' => true,
+                'enabled' => false,
                 'cache_history_range' => '1 year',
                 'cache_aggregations' => false,
             ],
@@ -373,7 +373,11 @@ class KlaviyoDriver implements SyncDriverInterface
      */
     public function validateConfig(array $config): array
     {
-        return $config;
+        return \Anibalealvarezs\ApiDriverCore\Services\ConfigSchemaRegistryService::hydrate(
+            $this->getChannel(),
+            'global',
+            $config
+        );
     }
 
     /**
